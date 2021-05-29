7ae9043991b5598ed4eb8e7de44232cce02dc218
SushantBait <53819730+SushantBait@users.noreply.github.com>
2021-05-29 14:11:09 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,21 +1,25 @@
 
 public class Test {
-    protected static final int MAX_LENGTH = 10;
+	private static java.util.Random r = new java.util.Random();
+	
+    static int MAX_LENGTH ;
  
-    private static java.util.Random r = new java.util.Random();
     private static final String DIGITS = "0123456789";
     private static final String LOCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
     private static final String UPCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String SYMBOLS = "!@#%*$[](){}";
+    
     private static final String ALL = DIGITS + LOCASE_CHARACTERS + UPCASE_CHARACTERS + SYMBOLS;
+ 
     private static final char[] upcaseArray = UPCASE_CHARACTERS.toCharArray();
     private static final char[] locaseArray = LOCASE_CHARACTERS.toCharArray();
     private static final char[] digitsArray = DIGITS.toCharArray();
     private static final char[] symbolsArray = SYMBOLS.toCharArray();
+    
     private static final char[] allArray = ALL.toCharArray();
  
 
-    public static String generatePassword() {
+    public static String generatePassword(int MAX_LENGTH) {
         StringBuilder sb = new StringBuilder();
          sb.append(locaseArray[r.nextInt(locaseArray.length)]);
          sb.append(upcaseArray[r.nextInt(upcaseArray.length)]);
@@ -29,8 +33,8 @@ public class Test {
     }
  
    public static void main(String[] args) {
-        
-       String thePassword = Test.generatePassword();
+	   MAX_LENGTH=10;
+       String thePassword = Test.generatePassword(MAX_LENGTH);
        System.out.println("Auto-Generated password is: " + thePassword);
    }
 }
